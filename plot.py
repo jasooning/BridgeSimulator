fixed Q calculation... fixed width calculation... fixed everything actually

plot.py
@@ -39,12 +39,22 @@ def plot(data_rows, plotting_fos):
     plt.figure(figsize=(12, 7))
 
     if plotting_fos:
-        # All series on same (log) axis
+    # All series on same (log) axis
         for i, y in enumerate(y_series):
             plt.plot(x, y, label=y_labels[i])
         plt.yscale("log")
+        plt.ylim(top = 10**9)
         plt.ylabel("FOS (log scale)")
         plt.axhline(y=1, color='red', linestyle='--', linewidth=1, label='FOS = 1')
+
+        # --- Highlight region using smallest series at each x ---
+        y_array = np.array(y_series)  # shape: (num_series, num_points)
+        min_series_idx = np.argmin(y_array, axis=0)  # index of smallest series at each x
+
+        for xi, idx in zip(x, min_series_idx):
+            # vertical line region (just a narrow rectangle around xi)
+            plt.fill_betweenx(plt.ylim(), xi-0.5, xi+0.5, color=plt.gca().lines[idx].get_color(), alpha=0.1)
+
     else:
         # Two-column special case: left/right axes
         #if len(y_series) != 2:
@@ -82,7 +92,6 @@ def plot(data_rows, plotting_fos):
 
         plt.xticks(np.arange(0, max(x)+50, 100))
         plt.tight_layout()
-        plt.show()
         return  # exit early since already plotted
 
     # --- Common plotting for FOS case ---